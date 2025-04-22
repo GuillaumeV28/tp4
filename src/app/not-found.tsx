2fd530cea2a9063ce5a318cc116eef853d361fa1
Guillaume <135722971+GuillaumeV28@users.noreply.github.com>
2025-04-22 18:34:23 +0200

    ajout not-found
    ajoue fichier not found.tsx pour deloyer

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>404 - Page non trouvée</h1>
+        <p>Oops, cette page n'existe pas.</p>
+      </div>
+    );
+  }
+  
